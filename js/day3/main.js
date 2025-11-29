11b45ab0310df77b63f3d37d2ba77679b700e56a
Rishi <awdhaw84@gmail.com>
2025-11-29 21:58:46 +0530

    Create main.js

---
+++ js/day3/main.js
@@ -0,0 +1,12 @@
+let b = "rishi "
+const inaz = "000"
+
+/// var a = 50; use less as possible
+/// inaz = "1" --> const named varible can be created once for the whole code
+{
+    let b = "this"
+    console.log(b)
+}
+
+console.log(b)
+console.log(inaz)
