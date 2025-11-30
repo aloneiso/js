1958a46da3977b290b1d6fdce28ec4277995ea66
Rishi <awdhaw84@gmail.com>
2025-12-01 02:30:10 +0530

    Create main.js

---
+++ js/day4/main.js
@@ -0,0 +1,22 @@
+// nn bb ss uu  
+
+// Primitives  in js 
+let a = null;
+let b = 55;
+let c = true; 
+let d = BigInt("55");
+let f = Symbol("hello");
+let g = undefined
+console.log(a,b,c,d,f,g)
+console.log(typeof g)
+
+//////////////
+//object in js 
+
+const item={
+    "harry": true,
+    "mohit": false,
+    "rohan": 45,
+    "shubham": "hello"
+}
+console.log(item["harry"])
