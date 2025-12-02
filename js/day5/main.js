111ed1d3a6fa196c9b0c85f0da6d9d5261bd2544
Rishi <awdhaw84@gmail.com>
2025-12-02 18:30:59 +0530

    Create main.js

---
+++ js/day5/main.js
@@ -0,0 +1,14 @@
+let a = "rishi"
+let b = 56;
+console.log(a + b)
+
+console.log(typeof a+b)
+
+const disct = {
+    name: "Rishi",
+    age: 22
+}
+console.log(disct)
+
+console.log(typeof disct)   
+console.log(disct.name)
